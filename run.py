581fa0b6def806fe4a578adaae8139000d754ded
kirimaks <kirimaks@yahoo.com>
2016-06-27 19:10:12 +0300

    app context

--- run.py
+++ run.py
@@ -1,26 +1,21 @@
 from weather_charts import app
 from flask_script import Manager
-from celery import Celery
 
 from jobs.scraper_job import scraper_job
 import time
 
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-celery.conf.update(app.config)
-
-manager = Manager(app)
-
-
-@celery.task
-def scraper(n):
+def run_job(n):
     while True:
         scraper_job()
         time.sleep(n)
 
-scraper(10)
+
+manager = Manager(app)
+
+with app.app_context():
+    run_job(60)
+
 
 if __name__ == "__main__":
     manager.run()
